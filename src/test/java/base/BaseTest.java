c54e38e6e09b46a1c92b7961459e948fec773db4
Vikas N K <vikas.nk@iqvia.com>
2025-06-26 08:15:28 +0530

    Added chrome options

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -3,6 +3,7 @@ package base;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.*;
 
 public class BaseTest {
@@ -15,7 +16,15 @@ public class BaseTest {
 
     @BeforeMethod
     public void setupTest() {
-        driver = new ChromeDriver();
+    	ChromeOptions options = new ChromeOptions();
+    	options.addArguments("--headless"); 
+    	options.addArguments("--no-sandbox");
+    	options.addArguments("--disable-dev-shm-usage");
+    	options.addArguments("--disable-gpu"); 
+    	options.addArguments("--remote-allow-origins=*");
+    	options.addArguments("--window-size=1920,1080");
+    	
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.get("https://practicetestautomation.com/practice-test-login/");
     }
