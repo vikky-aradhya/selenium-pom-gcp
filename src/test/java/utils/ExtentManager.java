1dd56cf99bbbc070061ab68b68cade1e690d7488
Vikas N K <vikas.nk@iqvia.com>
2025-06-29 17:23:39 +0530

    Added Utility

---
+++ src/test/java/utils/ExtentManager.java
@@ -0,0 +1,20 @@
+package utils;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentManager {
+    private static ExtentReports extent;
+
+    public static ExtentReports getInstance() {
+        if (extent == null) {
+            String reportPath = System.getProperty("user.dir") + "/test-output/ExtentReport.html";
+            ExtentSparkReporter spark = new ExtentSparkReporter(reportPath);
+            extent = new ExtentReports();
+            extent.attachReporter(spark);
+            extent.setSystemInfo("Environment", "QA");
+            extent.setSystemInfo("Tester", "Akash");
+        }
+        return extent;
+    }
+}
