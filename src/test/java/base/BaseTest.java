80929216eebfd8bbd7928b012b93d6e7aeb43dc7
Vikas N K <vikas.nk@iqvia.com>
2025-08-27 19:34:16 +0530

    Updated Parallel Execution

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -11,9 +11,9 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 
 public class BaseTest {
-    protected WebDriver driver;
+    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
     protected static ExtentReports extent;
-    protected static ExtentTest test;
+    private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
     
     @BeforeSuite
     public void setUpReport() {
@@ -28,27 +28,43 @@ public class BaseTest {
     @BeforeMethod
     public void setupTest() {
     	ChromeOptions options = new ChromeOptions();
-    	options.addArguments("--headless"); 
+    	//options.addArguments("--headless"); 
     	options.addArguments("--no-sandbox");
     	options.addArguments("--disable-dev-shm-usage");
     	options.addArguments("--disable-gpu"); 
     	options.addArguments("--remote-allow-origins=*");
     	options.addArguments("--window-size=1920,1080");
     	
-        driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
-        driver.get("https://practicetestautomation.com/practice-test-login/");
+        WebDriver localDriver = new ChromeDriver(options);
+        driver.set(localDriver);
+        
+        getDriver().manage().window().maximize();
+        getDriver().get("https://practicetestautomation.com/practice-test-login/");
     }
 
     @AfterMethod
     public void teardown() {
-        if (driver != null) {
-            driver.quit();
+        if (getDriver() != null) {
+        	getDriver().quit();
+        	driver.remove();
         }
+        test.remove();
     }
     
     @AfterSuite
     public void tearDownReport() {
         extent.flush(); // Very important
+    }
+    
+    public WebDriver getDriver() {
+        return driver.get();
+    }
+
+    public void setTest(ExtentTest extentTest) {
+        test.set(extentTest);
+    }
+
+    public ExtentTest getTest() {
+        return test.get();
     }
 }
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -10,15 +10,15 @@ public class LoginTest extends BaseTest {
 
     @Test
     public void testLoginSuccess() {
-    	test = extent.createTest("testLoginSuccess");
-        LoginPage loginPage = new LoginPage(driver);
+    	setTest(extent.createTest("testLoginSuccess - " + Thread.currentThread().getId()));
+        LoginPage loginPage = new LoginPage(getDriver());
         loginPage.login("student", "Password123");
         
         try {
             Assert.assertTrue(loginPage.getLoginMessage().contains("Congratulations"));
-            test.pass("Login successful");
+            getTest().pass("Login successful");
         } catch (AssertionError e) {
-            test.fail("Login failed");
+            getTest().fail("Login failed");
             throw e;
         }
     }
